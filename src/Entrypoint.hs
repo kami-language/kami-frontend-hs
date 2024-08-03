7962a388b7781f43ad6c127f2f3aae3ecb44d966
Maxim Urschumzew <u.maxim@live.de>
2024-08-03 23:18:23 +0200

    Add term parsing (WIP).

--- src/Entrypoint.hs
+++ src/Entrypoint.hs
@@ -1,19 +1,27 @@
 module Entrypoint where
 
 import Text.Parsec
+import Control.Monad
 
 
-type Toplevel = [Statement] 
+type Toplevel = [Statement]
 data Statement =
-    TypeDef String TypeVal 
-    | TermDef String TermVal
+    TypeDef Name TypeVal
+    | TermDef Name TermVal
+    deriving stock (Eq, Show)
 
 data Modality = At | Box
     deriving stock (Eq, Show)
 
 data TypeVal = Fun TypeVal TypeVal | Prod TypeVal TypeVal | Modal Modality TypeVal | Sum TypeVal TypeVal | List TypeVal | Unit
     deriving stock (Eq, Show)
-data TermVal = Var String | Lam | App | Fst | Snd | MkProd | Left' | Right' | Either | Nil | Cons | ListRec | TT
+data TermVal =
+    Var String
+    | Lam [FunArg] TermVal | App TermVal TermVal
+    | Fst TermVal | Snd TermVal | MkProd TermVal TermVal
+    | Left' TermVal | Right' TermVal | Either' TermVal TermVal TermVal
+    | Nil | Cons | ListRec
+    | TT
     deriving stock (Eq, Show)
 
 -- Implementation of infix operators with binding strength and fixity by
@@ -21,7 +29,11 @@ data TermVal = Var String | Lam | App | Fst | Snd | MkProd | Left' | Right' | Ei
 -- data TypeOp = FunOp | ProdOp | SumOp
 -- data TypeList = TNil | TCons 
 
+newtype Name = Name {getName :: String}
+    deriving newtype (Eq, Show)
 
+data FunArg = FunArg Name TypeVal
+    deriving stock (Eq, Show)
 
 stringParser:: Parsec String st String
 stringParser = many anyChar
@@ -29,18 +41,95 @@ stringParser = many anyChar
 white :: Parsec String st ()
 white = spaces
 
+spaces' :: Parsec String st ()
+spaces' = space >> spaces
+
+nameParser :: Parsec String st Name
+nameParser = Name <$> many1 alphaNum
+
 basetypeParser:: Parsec String st TypeVal
 basetypeParser = string "Unit" *> return Unit
     <|> between (string "(") (string ")") typeParser
 
 typeParser:: Parsec String st TypeVal
-typeParser = 
+typeParser =
     basetypeParser <* spaces >>= \x -> choice
-     [ eof >> return x
-     , lookAhead (string ")") >> return x
-     , string "->" >> spaces >> typeParser >>= \y -> return (Fun x y)
+     [ string "->" >> spaces >> typeParser >>= \y -> return (Fun x y)
      , string "," >> spaces >> typeParser >>= \y -> return (Prod x y)
+     , return x
+    --  , lookAhead (string ")") >> return x
      ]
+
+argParser :: Parsec String st FunArg
+argParser = between (string "(") (string ")")
+    (FunArg <$> nameParser <* spaces <* string ":" <* spaces <*> typeParser)
+
+basetermParser:: Parsec String st TermVal
+basetermParser = choice
+    [ string "tt" >> return TT
+    , string "\\" >> Lam <$> many1 (argParser <* spaces) <* string "->" <* spaces <*> termParser
+    , try $ between (string "(") (string ")") termParser
+
+
+    , string "fst" >> spaces' >> Fst <$> termParser
+    , string "snd" >> spaces' >> Snd <$> termParser
+    -- , try $ between (string "(") (string ")") (MkProd <$> termParser <* spaces <* string "," <* spaces <*> termParser)
+
+    , string "left" >> spaces' >> Left' <$> termParser
+    , string "right" >> spaces' >> Right' <$> termParser
+    , string "either" >> spaces' >> Either' <$> termParser <* spaces' <*> termParser <* spaces' <*> termParser
+    ]
+
+
+appParser:: Parsec String st TermVal
+-- appParser = g <$> basetermParser `sepBy` spaces'
+appParser = f <$> basetermParser <*> many (try (spaces' *> basetermParser))
+    where
+        -- g [] = undefined
+        -- g (x : xs) = f x xs
+
+        f acc [] = acc
+        f acc (x : xs) = f (App acc x) xs
+
+appParser':: TermVal -> Parsec String st TermVal
+appParser' acc' = f acc' <$> many (try (spaces' *> basetermParser))
+    where
+        f acc [] = acc
+        f acc (x : xs) = f (App acc x) xs
+
+
+-- termParser1:: Parsec String st TermVal
+-- termParser1 = choice
+--     [ string "fst" >> spaces' >> Fst <$> termParser
+--     , string "snd" >> spaces' >> Snd <$> termParser
+--     , string "left" >> spaces' >> Left' <$> termParser
+--     , string "right" >> spaces' >> Right' <$> termParser
+--     , string "either" >> spaces' >> Either' <$> termParser <* spaces' <*> termParser <* spaces' <*> termParser
+--     ]
+
+termParser:: Parsec String st TermVal
+termParser = basetermParser >>= \x -> choice
+    [ appParser' x <* (eof <|> lookAhead (void (string ")")))
+    , MkProd x <$ spaces <* string "," <* spaces <*> termParser
+    ]
+
+-- termParser:: Parsec String st TermVal
+-- termParser = choice 
+--     [ string "fst" >> spaces' >> Fst <$> termParser
+--     , string "snd" >> spaces' >> Snd <$> termParser
+--     , try $ between (string "(") (string ")") (MkProd <$> termParser <* spaces <* string "," <* spaces <*> termParser)
+
+--     , string "left" >> spaces' >> Left' <$> termParser
+--     , string "right" >> spaces' >> Right' <$> termParser
+--     , string "either" >> spaces' >> Either' <$> termParser <* spaces' <*> termParser <* spaces' <*> termParser
+
+--     , appParser
+--     ]
+
+statementParser :: Parsec String st Statement
+statementParser = TypeDef <$> nameParser <* spaces <* string ":"  <* spaces <*> typeParser
+    <|> TermDef <$> nameParser <* spaces <* string "="  <* spaces <*> termParser
+
 
     -- Fun <$> typeParser <* spaces <* string "->" <* spaces <*> typeParser
     -- <|> (string "Unit" *> return Unit)
--- test/EntrypointSpec.hs
+++ test/EntrypointSpec.hs
@@ -8,16 +8,48 @@ import Text.Parsec ( parse )
 test p = parse p ""
 
 spec :: Spec
-spec = describe "main" $ do
-    it "can parse Arrow types" $ do
-        test typeParser "Unit" `shouldBe` Right Unit
-        test typeParser "Unit -> Unit" `shouldBe` Right (Fun Unit Unit)
-        test typeParser "Unit -> Unit -> Unit" `shouldBe` Right (Fun Unit (Fun Unit Unit))
+spec = do
+    describe "typeParser" $ do
+        it "can parse Arrow types" $ do
+            test typeParser "Unit" `shouldBe` Right Unit
+            test typeParser "Unit -> Unit" `shouldBe` Right (Fun Unit Unit)
+            test typeParser "Unit -> Unit -> Unit" `shouldBe` Right (Fun Unit (Fun Unit Unit))
 
-    it "can parse Product types" $ do
-        test typeParser "Unit , Unit" `shouldBe` Right (Prod Unit Unit)
-        test typeParser "Unit , Unit , Unit" `shouldBe` Right (Prod Unit (Prod Unit Unit))
+        it "can parse Product types" $ do
+            test typeParser "Unit , Unit" `shouldBe` Right (Prod Unit Unit)
+            test typeParser "Unit , Unit , Unit" `shouldBe` Right (Prod Unit (Prod Unit Unit))
 
-    it "can parse Parenthesis in types" $ do
-        test typeParser "Unit -> Unit , Unit" `shouldBe` Right (Fun Unit (Prod Unit Unit))
-        test typeParser "(Unit -> Unit) , Unit" `shouldBe` Right (Prod (Fun Unit Unit) Unit)
+        it "can parse Parenthesis in types" $ do
+            test typeParser "Unit -> Unit , Unit" `shouldBe` Right (Fun Unit (Prod Unit Unit))
+            test typeParser "(Unit -> Unit) , Unit" `shouldBe` Right (Prod (Fun Unit Unit) Unit)
+
+    describe "termParser" $ do
+        it "can parse tt" $ do
+            test termParser "tt" `shouldBe` Right TT
+
+        it "can parse lambdas" $ do
+            test termParser "\\(f : Unit -> Unit) -> tt" `shouldBe` Right (Lam [FunArg (Name "f") (Fun Unit Unit)] TT)
+            test termParser "\\(f : Unit -> Unit) (a : Unit) -> tt" `shouldBe` Right (Lam [FunArg (Name "f") (Fun Unit Unit), FunArg (Name "a") Unit] TT)
+
+        it "can parse function applications" $ do
+            test termParser "tt tt" `shouldBe` Right (App TT TT)
+            test termParser "tt (tt tt tt)" `shouldBe` Right (App TT (App (App TT TT) TT))
+            test termParser "(fst tt) (snd tt)" `shouldBe` Right
+                (App (Fst TT) (Snd TT))
+
+        it "can parse product terms" $ do
+            test termParser "fst tt" `shouldBe` Right (Fst TT)
+            test termParser "tt , tt" `shouldBe` Right (MkProd TT TT)
+            test termParser "(tt , tt)" `shouldBe` Right (MkProd TT TT)
+            test termParser "snd (tt , tt)" `shouldBe` Right (Snd (MkProd TT TT))
+            test termParser "(fst tt) (snd tt , tt)" `shouldBe` Right
+                (App (Fst TT) (MkProd (Snd TT) TT))
+            test termParser "(tt , (fst tt) (snd tt , tt))" `shouldBe` Right
+                (MkProd TT (App (Fst TT) (MkProd (Snd TT) TT)))
+
+
+    describe "statementParser" $ do
+        it "can parse typedefs" $ do
+            test statementParser "hello : Unit -> Unit" `shouldBe`
+                Right (TypeDef (Name "hello") (Fun Unit Unit))
+
